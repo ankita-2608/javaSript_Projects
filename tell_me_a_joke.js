8d0e052b456bd09f768ac0cdeab485766e944b67
Ankita Raj <118102456+ankita-2608@users.noreply.github.com>
2023-03-28 19:19:06 +0530

    Add files via upload

---
+++ tell_me_a_joke.js
@@ -0,0 +1,117 @@
+let arr = [ "What is brown, hairy and wears sunglasses? A coconut on vacation." ,
+
+"What do you call someone with no body and no nose? Nobody knows." ,
+
+"Can a kangaroo jump higher than the Empire State Building? Of course. The Empire State Building can’t jump." ,
+
+"Why can’t a nose be 12 inches long? Because then it would be a foot." ,
+
+"What does a cloud wear under his raincoat? Thunderwear." ,
+
+"Did you hear about the mathematician who’s afraid of negative numbers? He’ll stop at nothing to avoid them." ,
+
+"Why didn’t Luke cross the road? He got a ticket for Skywalking!" ,
+
+"What did the duck say when he bought lipstick? Put it on my bill." ,
+
+"What do you call a dinosaur that is sleeping? A dino-snore!" ,
+
+"What do you call a magic Labrador? A Labracadabdoor!" ,
+
+"Doctor, doctor! They’ve dropped me from the cricket team – they call me butterfingers…Don’t worry, what you have is not catching!" ,
+
+"I don’t trust stairs. They’re always up to something." ,
+
+"Why don’t eggs tell jokes? They’d crack each other up." ,
+
+"Why can’t Elsa from Frozen have a balloon? Because she will “let it go, let it go.”" ,
+
+"How do you get a squirrel to like you? Act like a nut." ,
+
+"My grandfather has the heart of a lion and a lifetime ban at the zoo." ,
+
+"What happens to a frog’s car when it breaks down? It gets toad away." ,
+
+"What has ears but cannot hear? A cornfield." ,
+
+"Hear about the new restaurant called Karma? There’s no menu: You get what you deserve." ,
+
+"Helvetica and Times New Roman walk into a bar. “Get out of here!” shouts the bartender. “We don’t serve your type.”" ,
+
+"How many times can you subtract 10 from 100? Once. The next time you would be subtracting 10 from 90." ,
+
+"I invented a new word: Plagiarism!" ,
+
+"What insect is the most untidy? The litterbug!" ,
+
+"What is fast, loud and crunchy? A rocket chip!" ,
+
+"What did the left eye say to the right eye? Between us, something smells!" ,
+
+"How many Germans does it take to screw in a lightbulb? One, they’re efficient and not very funny." ,
+
+"What musical instrument is found in the bathroom? A tuba toothpaste." ,
+
+"My wife accused me of being immature. I told her to get out of my fort." ,
+
+"What did one plate say to the other plate? Dinner is on me!" ,
+
+"Parallel lines have so much in common. It’s a shame they’ll never meet." ,
+
+"“Dad, can you put my shoes on?” “No, I don’t think they’ll fit me.”" ,
+
+"Why did the dinosaur cross the road? Because the chicken wasn’t born yet." ,
+
+"Did you hear the rumor about butter? Well, I’m not going to spread it!" ,
+
+"Some guy called me a tool. So I got hammered and nailed his girlfriend. Guess he was right." ,
+
+"Someone stole my mood ring, I don’t know how I feel about that." ,
+
+"Sometimes I tuck my knees into my chest and lean forward. That’s just how I roll." ,
+
+"Why did the student eat his homework? Because the teacher told him it was a piece of cake!" ,
+
+"Why did the teddy bear say no to pudding? Because he was stuffed!" ,
+
+"What do you think of that new diner on the moon? The food was good, but they’re really wasn’t much atmosphere." ,
+
+"I threw a boomerang a few years ago. I now live in constant fear." ,
+
+"What time is it when the clock strikes 13? Time to get a new clock." ,
+
+"What did one toilet say to the other? You look a bit flushed." ,
+
+"I told my wife she was drawing her eyebrows too high. She looked surprised." ,
+
+"Two pickles fell out of a jar onto the floor. What did one say to the other? Dill with it." ,
+
+"What’s the best thing about Switzerland? I don’t know, but the flag is a big plus." ,
+
+"Where do the Avengers grab their super-skinny lattes? Starkbucks!" ,
+
+"My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right!" ,
+
+"I wish I had a book about boomerangs…I lent out my last one but it never came back!" ,
+
+"Why do we tell actors to “break a leg?” Because every play has a cast." ,
+
+"Women only call me ugly until they find out how much money I make. Then they call me ugly and poor." ,
+
+
+"Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?”" ,
+
+"What do you find in Superman’s bathroom? Superbowl!" ,
+
+"Yo momma so dumb, she tried to surf the microwave." ,
+
+
+"Yo mama so ugly, she made my Happy Meal cry.",
+
+"Yo mama so fat, she doesn’t need internet, she’s already worldwide.",
+
+"You’re not completely useless, you can always serve as a bad example." ]
+
+let a = Math.floor(Math.random() * arr.length-1)
+
+console.log(arr[a])
